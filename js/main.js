acd22062630e2f30e34c30dd59f8798202208dba
khoandptit-svg <khoand.ptit@gmail.com>
2026-05-22 16:23:56 +0700

    fix main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,16 @@ function renderProducts(data = products) {
     productList.innerHTML = ""; 
     
     data.forEach(product => {
+        // Tạo chuỗi HTML cho mỗi thẻ sản phẩm
+        // Thay đổi onclick thành thẻ <a> trỏ đến trang chi tiết kèm ID
         productList.innerHTML += `
             <div class="product-card">
-                <img src="${product.image}" onclick="openProductModal(${product.id})" style="cursor:pointer">
-                <h3 onclick="openProductModal(${product.id})" style="cursor:pointer">${product.name}</h3>
+                <a href="product-detail.html?id=${product.id}" style="text-decoration: none; color: inherit;">
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                </a>
                 <p class="price">${product.price.toLocaleString('vi-VN')} đ</p>
-                <button onclick="addToCart(${product.id})">Thêm vào giỏ</button>
+                <button class="add-to-cart-btn" onclick="addToCart(${product.id})">Thêm vào giỏ</button>
             </div>`;
     });
 }
