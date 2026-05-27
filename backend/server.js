1a206185ec334c8d2f03f0ea40ffa9dd932f1a25
NTKAn <anntk.ptit@gmail.com>
2026-05-27 20:42:26 +0700

    Thêm backend

---
+++ backend/server.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// DỮ LIỆU SẢN PHẨM
+const products = [
+    {
+        id: 1,
+        name: "Nồi Chiên Không Dầu Magic Eco AC-110",
+        price: 1850000,
+        oldPrice: 2500000,
+        image: "https://magic.com.vn/wp-content/uploads/2021/04/Noi-chien-khong-dau-MAGIC-Eco-AC-110_MAGIC-Vietnam_0922-1.png",
+        category: "noi-chien",
+        description: "Dung tích 6.5L, công nghệ Rapid Air giảm 80% dầu mỡ."
+    },
+    {
+        id: 2,
+        name: "Máy Xay Sinh Tố Đa Năng Magic Eco AC-15",
+        price: 890000,
+        oldPrice: 1200000,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGg0L_9UT2r_VpM9dkXUjbLU_9ZxyJyOdfYg&s",
+        category: "may-xay",
+        description: "Công suất 500W, lưỡi dao inox 304 siêu bền."
+    },
+    {
+        id: 3,
+        name: "Bếp Từ Magic Eco AC-201",
+        price: 1250000,
+        oldPrice: 1600000,
+        image: "https://magic.com.vn/wp-content/uploads/2022/08/bep-dien-tu-MAGIC-Eco-AC-202_MAGIC-Vietnam_0822-2.png",
+        category: "bep-tu",
+        description: "Mặt kính Ceramic chịu nhiệt, phím điều khiển cảm ứng."
+    },
+    {
+        id: 4,
+        name: "Nồi Cơm Điện Tử Magic Eco AC-250",
+        price: 990000,
+        oldPrice: 1990000,
+        image: "https://magic.com.vn/wp-content/uploads/2024/03/9-6.jpg",
+        category: "noi-com",
+        description: "Công nghệ cao tần IH giúp cơm chín đều, thơm ngon."
+    },
+    {
+        id: 5,
+        name: "Quạt Đứng Magic A-254",
+        price: 1350000,
+        oldPrice: 1590000,
+        image: "https://magic.com.vn/wp-content/uploads/2026/02/Ban-sao-cua-Bai-post-cua-Showroom-2.jpg",
+        category: "quat",
+        description: "Nhiều chế độ gió, tiết kiệm điện năng."
+    },
+    {
+        id: 6,
+        name: "Bếp Đôi Hồng Ngoại Và Điện Tử Magic Eco AC-220",
+        price: 10900000,  
+        oldPrice: 16990000,
+        image: "https://magic.com.vn/wp-content/uploads/2021/04/Bep-Doi-Hong-Ngoai-Va-Dien-Tu-Magic-Eco-AC-220_MAGIC-Vietnam_0922-2-1.jpg",
+        category: "bep-tu",
+        description: "Công suất mạnh mẽ,tiện lợi."
+    },
+    {
+        id: 7,
+        name: "Nồi Chiên Nướng Không Dầu MAGIC Eco AC-102 ",
+        price: 2690000,
+        oldPrice: 3590000,
+        image: "https://magic.com.vn/wp-content/uploads/2021/04/Noi-chien-khong-dau-MAGIC-Eco-AC-102_MAGIC-Vietnam_0922-1.png",
+        category: "noi-chien",
+        description: "Dung tích cực lớn 5.5 Lít. Giảm 90% lượng dầu mỡ"
+    },
+    {
+        id: 8,
+        name: "Nồi Cơm Điện Tử MAGIC Eco AC-260",
+        price: 1690000,
+        oldPrice: 1990000,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR15fJn9_Kz-vKCWUQe9Ko1bNpiSxP1qD2z0Q&s",
+        category: "noi-com",
+        description: "Công nghệ cao tần IH giúp cơm chín đều, thơm ngon."
+    },
+    {
+        id: 9,
+        name: "Máy Xay Sữa Hạt MAGIC A-103",
+        price: 1790000,
+        oldPrice: 2290000,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaZmLUHMB5TFbR-IReMkjSHxVid18gsiWP9g&s",
+        category: "may-xay",
+        description: "Công nghệ xay nhuyễn, thơm ngon."
+    },
+    {
+        id: 10,
+        name: "Quạt Tuần Hoàn Không Khí MAGIC A-256",
+        price: 1570000,
+        oldPrice: 1990000,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGfAXQJdT9utPGsXHCrhFyB1d_sWf6bQCxYg&s",
+        category: "quat",
+        description: "xoay 4D đảo chiều gió, làm mát toàn diện."
+    }
+];
+
+
+// API PRODUCTS
+app.get("/products", (req, res) => {
+
+    res.json(products);
+
+});
+
+
+app.listen(3000, () => {
+
+    console.log("Server chạy tại http://localhost:3000");
+
+});
